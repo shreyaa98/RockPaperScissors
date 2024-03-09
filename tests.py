4071bf64ed71a52199565bbe128232f81ce091c4
Shreyaa Prakash <shreyaa.prakash2015@gmail.com>
2024-03-09 21:43:24 +0100

    Fix Test Cases

--- tests.py
+++ tests.py
@@ -1,9 +1,9 @@
 import os
-import matplotlib as mpl
+
+
 if os.environ.get('DISPLAY','') == '':
-    print('no display found. Using non-interactive Agg backend')
-    mpl.use('Agg')
-import matplotlib.pyplot as plt
+    print('no display found. Using :0.0')
+    os.environ.__setitem__('DISPLAY', ':0.0')
 
 import unittest
 from tkinter import *
